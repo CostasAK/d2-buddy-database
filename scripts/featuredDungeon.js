35336160194de6d4e8f7e94e87d5aa0f7ee3da2c
CostasAK <costas@kokke.eu>
2022-08-24 21:26:22 +0200

    fix: look for objective name instead of fixed hash

--- scripts/featuredDungeon.js
+++ scripts/featuredDungeon.js
@@ -7,26 +7,49 @@ let featuredDungeon = JSON.parse(
 );
 
 if (!(featuredDungeon?.schedule?.[0]?.end >= Date.now())) {
-  bungieApi("/Destiny2/Milestones/").then((data) => {
-    console.log(data?.["1742973996"]);
+  bungieApi(
+    "/Destiny2/Armory/Search/DestinyMilestoneDefinition/Weekly Dungeon Challenge/"
+  ).then(({ results }) => {
+    const dungeonMilestones = results?.results
+      ?.filter(
+        (objective) =>
+          objective?.displayProperties?.name === "Weekly Dungeon Challenge"
+      )
+      ?.map((objective) => objective.hash);
 
-    const start = new Date(data?.["1742973996"]?.startDate).getTime();
-    const end = new Date(data?.["1742973996"]?.endDate).getTime();
-    const period = end - start;
+    bungieApi("/Destiny2/Milestones/").then((data) => {
+      const milestones = dungeonMilestones
+        .map((milestone) => data?.[milestone])
+        ?.filter(
+          (milestone) =>
+            milestone?.activities?.[0]?.challengeObjectiveHashes?.length > 0
+        );
+      console.log(milestones);
 
-    bungieApi(
-      "/Destiny2/Manifest/DestinyActivityDefinition/" +
-        data?.["1742973996"]?.activities?.[0]?.activityHash
-    ).then((data) => {
-      const name = data?.displayProperties?.name;
-      console.log(name);
+      if (milestones.length === 1) {
+        const milestone = milestones[0];
 
-      featuredDungeon.schedule.unshift({ start, end, period, name });
+        const start = new Date(milestone?.startDate).getTime();
+        const end = new Date(milestone?.endDate).getTime();
+        const period = end - start;
 
-      writeFileSync(
-        "./data/featuredDungeon.json",
-        JSON.stringify(featuredDungeon)
-      );
+        bungieApi(
+          "/Destiny2/Manifest/DestinyActivityDefinition/" +
+            milestone?.activities?.[0]?.activityHash
+        ).then((data) => {
+          const name = data?.displayProperties?.name;
+          console.log(name);
+
+          featuredDungeon.schedule.unshift({ start, end, period, name });
+
+          console.log(featuredDungeon);
+
+          writeFileSync(
+            "./data/featuredDungeon.json",
+            JSON.stringify(featuredDungeon)
+          );
+        });
+      }
     });
   });
 }
