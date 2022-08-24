165bc363392f73329c1364dbafa483298a03cf7f
CostasAK <costas@kokke.eu>
2022-08-24 20:44:24 +0200

    feat: script to find the current featured dungeon and save it

---
+++ functions/query.js
@@ -0,0 +1,17 @@
+import axios from "axios";
+
+const bungie_api = axios.create({
+  baseURL: "https://www.bungie.net/Platform",
+  transformResponse: axios.defaults.transformResponse.concat((data) => {
+    return data?.Response;
+  }),
+  headers: {
+    Accept: "application/json",
+    "X-API-KEY": process.env.BUNGIE_API_KEY,
+  },
+});
+
+export const bungieApi = async (path, method = "GET", headers = {}) => {
+  const { data } = await bungie_api({ url: path, method, headers });
+  return data;
+};
---
+++ scripts/featuredDungeon.js
@@ -0,0 +1,33 @@
+import { readFileSync, writeFileSync } from "fs";
+
+import { bungieApi } from "../functions/query.js";
+
+let featuredDungeon = JSON.parse(
+  readFileSync("./data/featuredDungeon.json", "utf8")
+);
+console.log(featuredDungeon);
+
+if (!(featuredDungeon?.schedule?.[0]?.end < Date.now())) {
+  bungieApi("/Destiny2/Milestones/").then((data) => {
+    console.log(data?.["1742973996"]);
+
+    const start = new Date(data?.["1742973996"]?.startDate).getTime();
+    const end = new Date(data?.["1742973996"]?.endDate).getTime();
+    const period = end - start;
+
+    bungieApi(
+      "/Destiny2/Manifest/DestinyActivityDefinition/" +
+        data?.["1742973996"]?.activities?.[0]?.activityHash
+    ).then((data) => {
+      const name = data?.displayProperties?.name;
+      console.log(name);
+
+      featuredDungeon.schedule.unshift({ start, end, period, name });
+
+      writeFileSync(
+        "./data/featuredDungeon.json",
+        JSON.stringify(featuredDungeon)
+      );
+    });
+  });
+}
