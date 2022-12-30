886de04086e9a46f0829301d8da998e84257df39
CostasAK <costas@kokke.eu>
2022-12-30 19:21:56 +0100

    feat: handle the API being down with a warning instead of an error

--- scripts/featuredRaid.js
+++ scripts/featuredRaid.js
@@ -10,45 +10,52 @@ export const featuredRaid = () => {
   if (!(featuredRaidJson?.schedule?.[0]?.end >= Date.now())) {
     bungieApi(
       "/Destiny2/Armory/Search/DestinyObjectiveDefinition/Weekly Raid Challenge/"
-    ).then(({ results }) => {
-      const raidObjectives = results?.results
-        ?.filter(
-          (objective) =>
-            objective?.displayProperties?.name === "Weekly Raid Challenge"
-        )
-        ?.map((objective) => objective.hash);
-
-      bungieApi("/Destiny2/Milestones/").then((data) => {
-        const milestones = Object.values(data)?.filter((milestone) =>
-          raidObjectives?.some((objective) =>
-            milestone?.activities?.[0]?.challengeObjectiveHashes?.includes(
-              objective
-            )
+    )
+      .then(({ results }) => {
+        const raidObjectives = results?.results
+          ?.filter(
+            (objective) =>
+              objective?.displayProperties?.name === "Weekly Raid Challenge"
           )
-        );
+          ?.map((objective) => objective.hash);
 
-        if (milestones.length === 1) {
-          const milestone = milestones[0];
+        bungieApi("/Destiny2/Milestones/").then((data) => {
+          const milestones = Object.values(data)?.filter((milestone) =>
+            raidObjectives?.some((objective) =>
+              milestone?.activities?.[0]?.challengeObjectiveHashes?.includes(
+                objective
+              )
+            )
+          );
 
-          const start = new Date(milestone?.startDate).getTime();
-          const end = new Date(milestone?.endDate).getTime();
-          const period = end - start;
+          if (milestones.length === 1) {
+            const milestone = milestones[0];
 
-          bungieApi(
-            "/Destiny2/Manifest/DestinyActivityDefinition/" +
-              milestone?.activities?.[0]?.activityHash
-          ).then((data) => {
-            const name = data?.displayProperties?.name?.split(": ")?.[0];
+            const start = new Date(milestone?.startDate).getTime();
+            const end = new Date(milestone?.endDate).getTime();
+            const period = end - start;
 
-            featuredRaidJson.schedule.unshift({ start, end, period, name });
+            bungieApi(
+              "/Destiny2/Manifest/DestinyActivityDefinition/" +
+                milestone?.activities?.[0]?.activityHash
+            ).then((data) => {
+              const name = data?.displayProperties?.name?.split(": ")?.[0];
 
-            writeFileSync(
-              "./data/featuredRaid.json",
-              JSON.stringify(featuredRaidJson)
-            );
-          });
-        }
+              featuredRaidJson.schedule.unshift({ start, end, period, name });
+
+              writeFileSync(
+                "./data/featuredRaid.json",
+                JSON.stringify(featuredRaidJson)
+              );
+            });
+          }
+        });
+        console.log("success");
+      })
+      .catch((error) => {
+        error.response.status === 503
+          ? console.warn("Bungie API Down.")
+          : console.error(error);
       });
-    });
   }
 };
