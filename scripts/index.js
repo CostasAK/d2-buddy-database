a4b2f78069753740a9199f401ee2e7a3af74739a
CostasAK <costas@kokke.eu>
2023-04-01 00:46:24 +0200

    fix: forgot extension in import

--- scripts/index.js
+++ scripts/index.js
@@ -1,6 +1,6 @@
 import { featuredDungeon } from "./featuredDungeon.js";
 import { featuredRaid } from "./featuredRaid.js";
-import { stringVariables } from "./stringVariables";
+import { stringVariables } from "./stringVariables.js";
 
 featuredRaid();
 featuredDungeon();
